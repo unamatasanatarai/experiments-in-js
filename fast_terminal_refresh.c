4dde6ba8329ee3d7d06358f0f11a1b3a97d66de8
Unamata Sanatarai <unamatasanatarai@gmail.com>
2021-09-27 20:38:16 +0200

    WIP

--- fast_terminal_refresh.c
+++ fast_terminal_refresh.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-    int w = 20;
-    int h = 10;
-    char out[200];
-    char chars[] = "123\n";
-    char reset[] = "\e[0;0H";
-    char visible[] = "\e[?25h";
-    char invisible[] = "\e[?25l";
+
+int w = 80;
+int h = 40;
+char chars[] = "123\n";
+char reset[] = "\e[0;0H";
+char visible[] = "\e[?25h";
+char invisible[] = "\e[?25l";
 int off = 0;
-void calc(){
+
+void calc(char* out){
     int cnt = 0;
-    int ecnt = 0;
     int y = 0;
     int x = 0;
     for(y =0; y < h; y++){
         for(x = 0; x < w; x++){
-            out[cnt] = chars[(off+cnt)%3];
-            cnt++;
-            ecnt++;
+            cnt= y * w + x;
+            out[cnt] = chars[ (off+cnt)%3];
         }
-        out[cnt-1] = chars[3];
+        out[cnt] = *"\n";
     }
     off++;
 }
 
 int main() {
+    // +1 for NUL in chars arrays
+    char *out = malloc(sizeof(char)*w*h+1);
+    if (out == NULL) {printf("I FAILED"); exit(1);};
     fputs(invisible, stdout);
-    //fputs(reset, stdout);
-    //fputs(out, stdout);
     int i = 0;
-      for (i=0;i<100000;i++){
-calc();
+    for (i=0;i<100000;i++){
+        calc(out);
         fputs(reset, stdout);
-          fwrite(out, sizeof(char), sizeof(out), stdout);
-          fflush(stdout);
-      }
+        fputs(out, stdout);
+        //fwrite(out, sizeof(char), w*h, stdout);
+        fflush(stdout);
+    }
     fputs(visible, stdout);
+    free(out);
 
-   return 0;
+    return 0;
 }
